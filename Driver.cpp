63004f9be3c1978d9c24dc91bfabd25afdba0f3a
Brenton Klassen <midwestmicro@live.com>
2014-07-16 12:01:37 -0500

    Cleaned up driver

--- Driver.cpp
+++ Driver.cpp
@@ -7,13 +7,7 @@ int main()
 	Library myLib;
 	myLib.ReadPeriodicalsFromFile();
 	myLib.ReadEmployeesFromFile();
-	// these dont work
-	// myLib.buildPriorityQueues(Date("8/13/2014")); 
-	// myLib.buildPriorityQueues(Date("8/25/2014"));
-	// myLib.buildPriorityQueues(Date("7/25/2014"));
-	myLib.buildPriorityQueues(Date("2/28/2014"));
-	// myLib.buildPriorityQueues(Date("3/19/2014"));
-	// myLib.buildPriorityQueues(Date("10/13/2014"));
+	myLib.buildPriorityQueues(Date("8/13/2014")); 
     myLib.ExecuteSimulator();
 	
 	system("pause");
